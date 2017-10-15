6fe7234f23b856209e31d784cbbd35d0f76945ac
Rongxin Sun <sunrongxin7666@163.com>
2017-10-15 23:37:21 +0800

    任务执行；

--- code/src/net/jcip/examples/ThreadPerTaskExecutor.java
+++ code/src/net/jcip/examples/ThreadPerTaskExecutor.java
@@ -11,6 +11,7 @@ import java.util.concurrent.*;
  */
 public class ThreadPerTaskExecutor implements Executor {
     public void execute(Runnable r) {
+        //为每个任务分配一个新的线程
         new Thread(r).start();
-    };
+    }
 }
