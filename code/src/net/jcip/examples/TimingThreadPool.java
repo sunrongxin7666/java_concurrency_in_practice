be9317be91bdb7d6ad9ecd082a15a839ac42e664
sunrongxin7666 <sunrongxin7666@163.com>
2017-11-03 12:38:06 +0800

    ExecutorService 定制线程

--- code/src/net/jcip/examples/TimingThreadPool.java
+++ code/src/net/jcip/examples/TimingThreadPool.java
@@ -14,7 +14,11 @@ import java.util.logging.*;
 public class TimingThreadPool extends ThreadPoolExecutor {
 
     public TimingThreadPool() {
-        super(1, 1, 0L, TimeUnit.SECONDS, null);
+        super(1,
+                1,
+                0L,
+                TimeUnit.SECONDS,
+                null);
     }
 
     private final ThreadLocal<Long> startTime = new ThreadLocal<Long>();
@@ -26,6 +30,7 @@ public class TimingThreadPool extends ThreadPoolExecutor {
 
     protected void beforeExecute(Thread t, Runnable r) {
         super.beforeExecute(t, r);
+        //记录任务执行的开始时间
         log.fine(String.format("Thread %s: start %s", t, r));
         startTime.set(System.nanoTime());
     }
@@ -37,6 +42,7 @@ public class TimingThreadPool extends ThreadPoolExecutor {
             //原子性增长
             numTasks.incrementAndGet();
             totalTime.addAndGet(taskTime);
+            //记录任务结束时间和执行时间长度
             log.fine(String.format("Thread %s: end %s, time=%dns",
                     t, r, taskTime));
         } finally {
@@ -46,6 +52,7 @@ public class TimingThreadPool extends ThreadPoolExecutor {
 
     protected void terminated() {
         try {
+            //统计整个进程池在执行期间的平均执行时间
             log.info(String.format("Terminated: avg time=%dns",
                     totalTime.get() / numTasks.get()));
         } finally {
